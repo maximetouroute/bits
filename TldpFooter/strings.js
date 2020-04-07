d06330b7cc22ee8b8ad53114a073cb0272acabd3
Maxime Touroute <maxime.touroute@gmail.com>
2020-04-07 10:23:48 +0200

    tldp ui updates

---
+++ TldpFooter/strings.js
@@ -0,0 +1,17 @@
+export const strings = {
+
+    followUs: {
+        en: 'Follow us!',
+        fr: 'Suivez nous !'
+    },
+
+    moreContacts: {
+        en: 'More contacts',
+        fr: 'Plus de contacts'
+    },
+
+    contact: {
+        en: 'Contact',
+        fr: 'Nous contacter'
+    }
+}
