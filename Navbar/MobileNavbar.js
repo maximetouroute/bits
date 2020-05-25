2cc456610cfcf470d1e5a2b8194e7daa5090a782
Maxime Touroute <maxime.touroute@gmail.com>
2020-05-26 00:11:22 +0200

    misc theming

--- Navbar/MobileNavbar.js
+++ Navbar/MobileNavbar.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import { Link } from 'gatsby'
-import './Navbar.scss'
+import React from 'react';
+import { Link } from 'gatsby';
+import './Navbar.scss';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
@@ -10,15 +10,25 @@ import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
 
 const useStyles = makeStyles({
   root: {
-    boxShadow: '0 -1px 4px 0 rgba(99,99,99,.11)',
-    borderTop: 'solid 2px #f0f0f0',
-    color: 'red',
+    boxShadow: '0 -1px 4px 0 rgba(99,99,99,.11)'
   },
 });
+
+const useChildStyles = makeStyles({
+  root: {
+    color: '#5f7895',
+  },
+  selected: {
+    color: '#3568cf',
+    fontWeight: 600,
+    }
+
+})
 
 export default function MobileNavbar(props) {
   const {links} = props;
   const classes = useStyles();
+  const childClasses = useChildStyles();
   const [value, setValue] = React.useState(0);
 // TODO: to make something truly factorised, user shall be able to override ths method by injecting it in the component
 const populateIconForName = (name) => {
@@ -47,9 +57,10 @@ const populateIconForName = (name) => {
         key={link.path}
         label={link.path} 
         component={Link}
+        className={childClasses.root}
              to={link.path}
              label={link.name}
-             activeClassName="Mui-selected"
+             activeClassName={childClasses.selected}
              label={link.name}
              icon = {populateIconForName(link.name)}
              />
