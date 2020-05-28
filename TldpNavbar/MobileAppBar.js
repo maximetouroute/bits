16f9a06ec805e27b2723e183e26d84a92163d7a1
Maxime Touroute <maxime.touroute@gmail.com>
2020-05-28 22:58:21 +0200

    New : absolutely not factorised tldp navbar

---
+++ TldpNavbar/MobileAppBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import './Navbar.scss'
+export default function HideAppBar(props) {
+  return (
+    <div className="Navbar">
+ <div className="MobileAppBar">
+    <Link activeClassName="" className="siteTitle" to="/">
+    {props.title}
+  </Link>
+  </div>
+    </div>
+  );
+}
---
+++ TldpNavbar/Navbar.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { Link } from 'gatsby'
+import './Navbar.scss'
+import PropTypes from 'prop-types'
+import MobileNavbar from './MobileNavbar';
+import { NavbarSlim } from './../NavbarSlim/NavbarSlim';
+
+export default class Navbar extends Component {
+
+  mobileMenuItems() {
+    const links = this.props.links;
+    return <MobileNavbar links={links}></MobileNavbar>;
+  }
+  menuItems() {
+    const title = this.props.title
+    const links = this.props.links
+    return (
+      <>
+        <Link activeClassName="" className="siteTitle" to="/">
+          {title}
+        </Link>
+        {links.map(link => (
+          <Link activeClassName="active" to={link.path} key={link.name}>
+            {link.name}
+          </Link>
+        ))}
+      </>
+    )
+
+  }
+
+  render() {
+    return (
+      <div className="Navbar">
+      <div className="SmallNavbar">
+      {this.mobileMenuItems()}
+      </div>
+      <div className="BigNavbar">
+      <NavbarSlim links={this.props.links}/>
+      </div>
+      </div>
+    )
+  }
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+  links: PropTypes.array,
+}
+
+Navbar.defaultProps = {
+  title: 'SiteTitle',
+  links: [
+    { path: '/', name: 'rootLink' },
+    { path: '/somePath', name: 'somePage' },
+    { path: '/otherPath', name: 'otherPage' },
+  ],
+}
